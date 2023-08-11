d48186bad5e98c6953cef15a72ee1298ecd220ef
Anna Berezhna <anna.berejna@gmail.com>
2023-08-11 22:57:23 +0300

    camera and stickman position

--- main.js
+++ main.js
@@ -11,7 +11,8 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.z = 5;
+camera.position.set(2, 2, 5); // Позиція камери
+camera.lookAt(1, 3, 0); // Камера спрямована на центр сцени
 
 // Додавання рендерера
 const renderer = new THREE.WebGLRenderer();
@@ -27,32 +28,33 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(1, 1, 1);
 scene.add(directionalLight);
 
-// Завантаження моделей
+// Завантаження доріжки
 const loader = new GLTFLoader();
 
-let headModel, bodyModel;
+let trackModel, bodyModel;
 
-loader.load("static/Brain.glb", (gltf) => {
-  headModel = gltf.scene;
-  headModel.scale.set(0.1, 0.1, 0.1);
-  scene.add(headModel);
+loader.load("static/TrackFloor.glb", (gltf) => {
+  trackModel = gltf.scene;
+  trackModel.scale.set(1, 1, 3);
+  scene.add(trackModel);
+
+  // Позиціонування доріжки
+  trackModel.position.set(0, -1, 0);
+
+  // Позиціонування чоловічка на центрі доріжки
+  bodyModel.position.set(0, 0.5, 0); // Припустимо, що тіло на висоті 0.5
 });
 
 loader.load("static/Stickman.glb", (gltf) => {
   bodyModel = gltf.scene;
-  bodyModel.scale.set(0.1, 0.1, 0.1);
+  bodyModel.scale.set(0.6, 0.6, 0.6);
+  bodyModel.rotation.y = Math.PI; // Поворот тіла на 180 градусів (за ось Y)
   scene.add(bodyModel);
 });
 
 // Оновлення сцени та рендеринг
 function animate() {
   requestAnimationFrame(animate);
-
-  if (headModel && bodyModel) {
-    // Позиціонування та орієнтація моделей
-    
-  }
-
   renderer.render(scene, camera);
 }
 
