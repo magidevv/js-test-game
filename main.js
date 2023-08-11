11fbad6674025e03a38991a19eb7b292b43c967a
Anna Berezhna <anna.berejna@gmail.com>
2023-08-11 22:22:24 +0300

    add scene and stickman

--- main.js
+++ main.js
@@ -1,28 +1,59 @@
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
+// Створення сцени
 const scene = new THREE.Scene();
+
+// Створення камери
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
+camera.position.z = 5;
 
+// Додавання рендерера
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// Додавання освітлення
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
 
-camera.position.z = 5;
+// Додавання напрямленого світла
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(1, 1, 1);
+scene.add(directionalLight);
 
+// Завантаження моделей
+const loader = new GLTFLoader();
+
+let headModel, bodyModel;
+
+loader.load("static/Brain.glb", (gltf) => {
+  headModel = gltf.scene;
+  headModel.scale.set(0.1, 0.1, 0.1);
+  scene.add(headModel);
+});
+
+loader.load("static/Stickman.glb", (gltf) => {
+  bodyModel = gltf.scene;
+  bodyModel.scale.set(0.1, 0.1, 0.1);
+  scene.add(bodyModel);
+});
+
+// Оновлення сцени та рендеринг
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+
+  if (headModel && bodyModel) {
+    // Позиціонування та орієнтація моделей
+    
+  }
+
   renderer.render(scene, camera);
 }
+
 animate();
